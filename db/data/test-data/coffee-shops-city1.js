455aa416e57799a8b71d985f421b3a822b3f12a0
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-22 10:37:32 +0000

    coffee-shop object added to test data

---
+++ db/data/test-data/coffee-shops-city1.js
@@ -0,0 +1,13 @@
+[
+    {
+        name: "shop1",
+        longitude: 0,
+        latitude: 0,
+        city: "city1",
+        totalRatings: 10,
+        rating: 4.5,
+        dogFriendly: true,
+        dairyFree: false,
+        hasSeating: true,
+    }
+]

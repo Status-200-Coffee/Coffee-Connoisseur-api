a89dd733a9af1425cc3aa91f5638982ebd67a101
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-22 16:00:32 +0000

    GET /api/shops/:city/:shop_id endpoint added

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const Koa = require("koa");
-const Router = require('koa-router')
+const Router = require("koa-router");
+const { getShopById } = require("./controllers/coffee.controller");
 
 const app = new Koa();
-const router = new Router()
+const router = new Router();
 
-app.use(router.routes())
+app.use(router.routes());
 
-router.get('/', (ctx) => {ctx.body = "app is working"})
+router.get("/", (ctx) => {
+  ctx.body = "app is working";
+});
+
+router.get("/api/shops/:city/:shop_id", getShopById);
+
+router.use((err, ctx, next) => {
+  ctx.status = err.response.status;
+  ctx.body = err.response.message;
+});
 
 app.listen(9090, function () {
   console.log("Server running on https://localhost:9090");
 });
+
+module.exports = app;

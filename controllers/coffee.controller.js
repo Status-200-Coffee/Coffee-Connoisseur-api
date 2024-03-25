cd5a1fcedc6eff2a10f045e7d3eb2b85dc9a433d
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-25 11:04:52 +0000

    patchShopById controller added

--- controllers/coffee.controller.js
+++ controllers/coffee.controller.js
@@ -1,9 +1,20 @@
-const { findShopById } = require("../models/coffee.model");
+const { findShopById, updateShopById } = require("../models/coffee.model");
 
 exports.getShopById = async (ctx, next) => {
   const { city, shop_id } = ctx.params;
   try {
     const shop = await findShopById(city, +shop_id);
+    ctx.body = { shop };
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.patchShopById = async (ctx, next) => {
+  const { city, shop_id } = ctx.params;
+  const { rating, newPhoto } = ctx.request.body;
+  try {
+    const shop = await updateShopById(city, +shop_id, rating, newPhoto);
     ctx.body = { shop };
   } catch (error) {
     next(error);

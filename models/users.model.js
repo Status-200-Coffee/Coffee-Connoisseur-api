d54d11a44941eec610d4a70dab0005fe999ce9bf
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-25 16:07:22 +0000

    environment added to distinguish test and dev

--- models/users.model.js
+++ models/users.model.js
@@ -1,11 +1,16 @@
 const { client } = require("../db/connection");
+let dbName = "coffee-conneisseur-api";
+
+if (process.env.NODE_ENV === "test") {
+  dbName += "-test";
+}
 
 exports.findUsers = async () => {
     try {
       await client.connect();
   
       const result = await client
-        .db("coffee-conneisseur-api")
+        .db(dbName)
         .collection("users")
         .find()
         .toArray();

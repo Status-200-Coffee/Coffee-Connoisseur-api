ee05b7739e6cbfd9a9ec6be8a8f0e3b63f4c2ee8
carmeljane <carmelbarwick@gmail.com>
2024-03-26 10:50:00 +0000

    conflicts resolved

--- models/users.model.js
+++ models/users.model.js
@@ -21,28 +21,58 @@ exports.findUsers = async () => {
 };
 
 exports.updateUserByUsername = async (
-  username,
-  { newPhoto, changeCoffee, addToFavourites, removeFromFavourites }
-) => {
-  if (!newPhoto && !changeCoffee && !addToFavourites && !removeFromFavourites) {
-    throw new Error();
-  }
-  await client.connect();
-
-  const user = await client
-    .db(dbName)
-    .collection("users")
-    .findOne({ username });
-
-  const change = {};
-
-  if (newPhoto) {
-    if (typeof newPhoto !== "string") {
+    username,
+    { newPhoto, changeCoffee, addToFavourites, removeFromFavourites }
+  ) => {
+    if (!newPhoto && !changeCoffee && !addToFavourites && !removeFromFavourites) {
       throw new Error();
     }
-
+    await client.connect();
+    const user = await client
+      .db(dbName)
+      .collection("users")
+      .findOne({ username });
+    const change = {};
+    if (newPhoto) {
+      if (typeof newPhoto !== "string") {
+        throw new Error();
+      }
+      const photos = user.photosPosted;
+      photos.unshift(newPhoto);
+      change.photosPosted = photos;
+      user.photosPosted = photos;
+    }
+    if (changeCoffee) {
+      let coffee = user.coffeeCollected;
+      if (+changeCoffee === 1) {
+        coffee++;
+      }
+      if (+changeCoffee === -1) {
+        coffee = 0;
+      }
+      change.coffeeCollected = coffee;
+      user.coffeeCollected = coffee;
+    }
+    if (addToFavourites) {
+      const favourites = user.favouriteShops;
+      favourites.unshift(5);
+      change.favouriteShops = favourites;
+      user.favouriteShops = favourites;
+    }
+    if (removeFromFavourites) {
+      const favourites = user.favouriteShops;
+      const updatedFavourites = favourites.filter(
+        (shop) => shop !== removeFromFavourites
+      );
+      change.favouriteShops = updatedFavourites;
+      user.favouriteShops = updatedFavourites;
+    }
+    const updateUser = await client
+      .db(dbName)
+      .collection("users")
+      .updateOne({ username }, { $set: { change } });
+    return user;
   };
-
   exports.findUserByUsername = async (username) => {
     try {
       await client.connect();
@@ -64,46 +94,3 @@ exports.updateUserByUsername = async (
       await client.close();
     }
   };
-
-    const photos = user.photosPosted;
-    photos.unshift(newPhoto);
-    change.photosPosted = photos;
-    user.photosPosted = photos;
-  }
-
-  if (changeCoffee) {
-    let coffee = user.coffeeCollected;
-    if (+changeCoffee === 1) {
-      coffee++;
-    }
-    if (+changeCoffee === -1) {
-      coffee = 0;
-    }
-    change.coffeeCollected = coffee;
-    user.coffeeCollected = coffee;
-  }
-
-  if (addToFavourites) {
-    const favourites = user.favouriteShops;
-    favourites.unshift(5);
-    change.favouriteShops = favourites;
-    user.favouriteShops = favourites;
-  }
-
-  if (removeFromFavourites) {
-    const favourites = user.favouriteShops;
-    const updatedFavourites = favourites.filter(
-      (shop) => shop !== removeFromFavourites
-    );
-    change.favouriteShops = updatedFavourites;
-    user.favouriteShops = updatedFavourites;
-  }
-
-  const updateUser = await client
-    .db(dbName)
-    .collection("users")
-    .updateOne({ username }, { $set: { change } });
-
-  return user;
-};
-
